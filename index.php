eba5a27d1c3b751351c6a41cc8cb7f30edc22aab
blitzMN <mrstanton81@gmail.com>
2013-12-03 15:10:03 -0600

    ui assets/ layout styling
    basic structure to work with, now to fill in functionality

--- index.php
+++ index.php
@@ -6,26 +6,99 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">-->
     <link rel="stylesheet" type="text/css" href="css/main.css">
 
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
+    <!--<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>-->
     <script type="text/javascript" src="lib/js/load.js"></script>
 </head>
 <body>
-<div id="step1" class="upload_container">
-    <h1>UPLOAD IMAGE</h1>
-    <form method="post" enctype="multipart/form-data" action="lib/php/upload.php">  
-      <input type="file" name="images" id="images"/>  
-      <button type="submit" id="btnUpload">Upload Files!</button>  
-    </form>  
+<div class="logo"><!--GNC--></div>
+<div id="intro" class="intro container">
+    <div class="box intro">
+        <button id="btnStart"></button>
+    </div>
+</div>
+<div id="step1" class="upload container">
+    <!-- Upload and save image to server -->
+    <div class="box">
+        <ul class="nav_icons">
+            <li><img src="assets/images/icon_upload_on.png" width="28" height="28" alt="STEP ONE"/></li>
+            <li><img src="assets/images/icon_edit_off.png" width="31" height="27" alt="STEP TWO"/></li>
+            <li><img src="assets/images/icon_share_off.png" width="31" height="27" alt="STEP THREE"/></li>
+        </ul>
+        <div class="lbl_upload"></div>
+        <form method="post" enctype="multipart/form-data" action="lib/php/upload.php">  
+          <input type="file" name="images" id="images" class="btnUpload"/>  
+          <button type="submit" id="btnUpload">Click to Upload.</button>  
+        </form>  
+
+        
   
-    <div id="response"></div>  
-    <ul id="image-list">
+       <!--<div id="response"></div>
+       <ul id="image-list">-->
+        
+    </div>
 </div>  
 
-<div id="step2" class="edit_container">
-    <canvas id="canvas" width="640" height="640"/>
+<div id="step2" class="crop container">
+    <!-- Crop and save image -->
+    <div class="box">
+        <h3>STEP 2</h3>
+        <button class="previous"></button>
+        <button class="next"></button>
+    </div>
+</div>
+
+<div id="step3" class="filter container">
+    <!-- Loads cropped image as black and white with image overlay options -->
+    <div class="box">
+        <h3>STEP 3</h3>
+        <div class="color_tools_container">
+            <ul id="colorTools">
+                <li><img src="rad_orange_on" width="32" height="32" alt="ORANGE"/></li>
+                <li><img src="rad_red_off.png" width="32" height="32" alt="RED"/></li>
+                <li><img src="rad_purple_off.png" width="32" height="32" alt="PURPLE"/></li>
+                <li><img src="rad_blue_off.png" width="32" height="32" alt="BLUE"/></li>
+                <li><img src="rad_green_off.png" width="32" height="32" alt="GREEN"/></li>
+                <li><img src="rad_yellow_off.png" width="32" height="32" alt="YELLOW"/></li>
+            </ul>
+        </div>
+        <button class="previous"></button>
+        <button class="next"></button>
+    </div>
+</div>
+
+<div id="step4" class="form container">
+    <!-- Text form -->
+    <div class="box">
+        <h3>STEP 4</h3>
+        <button id="btnEnterTxt" class="btn_enter_txt"></button>
+    </div>
+</div>
+
+<div id="step5" class="text container">
+    <!-- Text placement on filtered image -->
+    <div class="box">
+        <h3>STEP 5</h3>
+        <button class="previous"></button>
+        <button class="next"></button>
+    </div>
+</div>
+
+<div id="step6" class="share container">
+    <!-- smoked share functionality -->
+    <div class="box">
+        <h3>STEP 6</h3>
+        <button id="btnShare"></button>
+    </div>
+</div>
+
+<div id="step7" class="final container">
+    <!-- display final image that would be shared. -->
+    <div class="box">
+        <h3>STEP 7</h3>
+    </div>
 </div>
 
 </body>
